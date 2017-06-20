ec78bb9c646534114cdc0a3de9db70d97338ff01
SirRade <jn_ferner@hotmail.de>
2017-06-20 14:09:14 +0200

    Change rocket to iron

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
-#![feature(plugin)]
-#![plugin(rocket_codegen)]
-extern crate serde;
-extern crate rocket;
+extern crate iron;
+#[macro_use]
+extern crate router;
 extern crate serde_json;
 #[macro_use]
-extern crate serde_derive;  
+extern crate serde_derive;
 
-use rocket::response::content::JSON;
+use iron::prelude::*;
+use iron::status;
+use router::Router;
 
-#[post("/sbb/telegram/update", format = "application/json", data = "<update>")]
-fn telegram_update(update: JSON<Update>) -> String {
-    "hi".to_owned()
+use std::io::Read;
+fn main() {   
+    let router = router!(telegram: post "/telegram" => telegram);
+
+    Iron::new(router).http("localhost:3000").unwrap();
+
+    fn telegram(req: &mut Request) -> IronResult<Response> {
+        let mut body = Vec::new();
+        req
+            .body
+            .read_to_end(&mut body)
+            .map_err(|e| IronError::new(e, (status::InternalServerError, "Error reading request")))?;
+        let body = String::from_utf8(body).unwrap();
+        println!("{}", body);
+        Ok(Response::with((status::Ok, "ok")))
+    }
 }
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
-}
-
-fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
-}
 
 #[derive(Serialize, Deserialize)]
 struct Update {
@@ -38,40 +44,58 @@ struct Update {
 
 #[derive(Serialize, Deserialize)]
 struct Message {
-
+    message_id: i32,
+    from: Option<User>,
+    date: i32,
+    chat: Chat,
+    forward_from: Option<User>,
+    forward_from_chat: Option<Chat>,
+    forward_from_message_id: Option<i32>,
+    forward_date: Option<i32>,
+    reply_to_message: Box<Option<Message>>,
+    edit_date: Option<i32>,
+    text: Option<String>,
+    entities: Option<Vec<MessageEntity>>,
+    audio: Option<Audio>,
+    document: Option<Document>,
+    game: Option<Game>,
+    photo: Option<Vec<PhotoSize>>,
+    sticker: Option<Sticker>,
+    video: Option<Video>,
+    voice: Option<Voice>,
+    video_note: Option<VideoNote>,
+    new_chat_members: Option<Vec<User>>,
+    caption: Option<String>,
+    contact: Option<Contact>,
+    location: Option<Location>,
+    venue: Option<Venue>,
+    new_chat_member: Option<User>,
+    left_chat_member: Option<User>,
+    new_chat_title: Option<String>,
+    new_chat_photo: Option<Vec<PhotoSize>>,
+    delete_chat_photo: Option<bool>,
 }
 
 #[derive(Serialize, Deserialize)]
-struct InlineQuery {
-
-}
+struct InlineQuery {}
 
 #[derive(Serialize, Deserialize)]
-struct ChosenInlineResult {
-
-}
+struct ChosenInlineResult {}
 
 #[derive(Serialize, Deserialize)]
-struct CallbackQuery {
-
-}
+struct CallbackQuery {}
 
 #[derive(Serialize, Deserialize)]
-struct ShippingQuery {
-
-}
+struct ShippingQuery {}
 
 #[derive(Serialize, Deserialize)]
-struct PreCheckoutQuery {
-
-}
+struct PreCheckoutQuery {}
 
 #[derive(Serialize, Deserialize)]
-struct User {
-
-}
+struct User {}
 
 #[derive(Serialize, Deserialize)]
-struct Chat {
-    
-}
+struct Chat {}
+
+#[derive(Serialize, Deserialize)]
+struct MessageEntity {}

5c69fcdc02a1272c926e903e49d0728e5bd7a88b
Lord-Curdin <lucuhu@gmail.com>
2017-06-20 13:35:20 +0200

    Implement Telegramtypes

--- src/main.rs
+++ src/main.rs
@@ -43,27 +43,68 @@ struct Message {
 
 #[derive(Serialize, Deserialize)]
 struct InlineQuery {
-
+    update_id: i32,  
+    from: User,
+    location: Option<Location>,
+    query: String,
+    offset: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct ChosenInlineResult {
-
+    update_id: i32,  
+    from: User,
+    location: Option<Location>,
+    inline_message_id: Option<String>,
+    query: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct CallbackQuery {
-
+    result: i32,  
+    from: User,
+    message: Option<String>,
+    inline_message_id: Option<String>,
+    chat_instance: String,
+    data: Option<String>,
+    game_short_name: Option<String>
 }
 
 #[derive(Serialize, Deserialize)]
 struct ShippingQuery {
+    shipping_query_id: String,
+    ok: bool,
+    shipping_options: Option<Vec<ShippingOption>>,
+    error_message: Option<String>,
+}
 
+#[derive(Serialize, Deserialize)]
+struct ShippingOption {
+    id:String,
+    from: User,
+    invoice_payload: String,
+    shipping_address: ShippingAddress,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ShippingAddress {
+   country_code: String,
+   state: String,
+   city: String,
+   street_line1: String,
+   street_line2: String,
+   post_code: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct PreCheckoutQuery {
-
+id:String,
+from: User,
+currency: String,
+total_amount: i32,
+invoice_payload: String,
+shipping_option_id: Option<String>,
+order_info: Option<OrderInfo>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -74,4 +115,15 @@ struct User {
 #[derive(Serialize, Deserialize)]
 struct Chat {
     
+}
+
+#[derive(Serialize, Deserialize)]
+struct Location {
+longitude: f64,
+latitude: f64,
+}
+
+#[derive(Serialize, Deserialize)]
+struct OrderInfo {
+
 }

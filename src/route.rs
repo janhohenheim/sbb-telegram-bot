6d4733679b11bf41a6ff1d8be4d9cbb8eb001e5d
SirRade <jn_ferner@hotmail.de>
2017-06-25 22:53:59 +0200

    Refactor sending

--- src/route.rs
+++ src/route.rs
@@ -21,27 +21,45 @@ pub fn telegram(req: &mut Request) -> IronResult<Response> {
     if let Some(msg) = update.message {
         if let Some(txt) = msg.text {
             let is_private = msg.chat.chat_type == "private";
-            let stated_privately = is_private && txt == "/start";
-            let started_with_identifier = txt == format!("/start@{}", read_bot_data(&BotData::Name));
-            
-            if stated_privately || started_with_identifier {
-                respond_start(msg.chat.id)?;
-            } else if is_private {
-                respond_unknown(msg.chat.id)?;
+            let identifier = format!("@{}", read_bot_data(&BotData::Name));
+            let has_identifier = txt.find(&identifier).is_some();
+            let txt = strip_identifier(&txt);
+            if is_private || has_identifier {
+                let id = msg.chat.id;
+                match txt.as_str() {
+                    "/start" => respond_start(id),
+                    _ => {
+                        if is_private {
+                            respond_unknown(id)
+                        } else {
+                            Ok(())
+                        }
+                    }
+                }?;
             }
         }
     }
     Ok(Response::with((status::Ok, "ok")))
 }
 
-fn respond_start(chat_id: i32) -> IronResult<reqwest::Response> {
+fn respond_start(chat_id: i32) -> IronResult<()> {
     send(chat_id,
          "If the bot was already working, \
                     you would now have registered yourself. \
                     Alas, as this is only a placeholder text, \
-                    nothing happened")
+                    nothing happened")?;
+    Ok(())
 }
 
-fn respond_unknown(chat_id: i32) -> IronResult<reqwest::Response> {
-    send(chat_id, "Unknown command. Try using /help")
+fn respond_unknown(chat_id: i32) -> IronResult<()> {
+    send(chat_id, "Unknown command. Try using /help")?;
+    Ok(())
+}
+
+fn strip_identifier(msg: &str) -> String {
+    let identifier = format!("@{}", read_bot_data(&BotData::Name));
+    if let Some(pos) = msg.find(&identifier) {
+        return msg[..pos].to_owned();
+    }
+    msg.to_owned()
 }

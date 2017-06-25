f06cdba679d386c132665007220b323addbb14b3
SirRade <jn_ferner@hotmail.de>
2017-06-25 18:59:29 +0200

    Refactor sending into general function

--- src/route.rs
+++ src/route.rs
@@ -4,10 +4,9 @@ extern crate serde_json;
 extern crate reqwest;
 
 use self::iron::prelude::*;
-use self::iron::status;
-use self::iron::{ Request, Response, IronResult};
+use self::iron::{status, Request, Response, IronResult};
 
-use super::util::{BotData, read_bot_data};
+use super::util::{BotData, read_bot_data, send};
 use super::model::telegram;
 
 use std::io::Read;
@@ -19,31 +18,32 @@ pub fn telegram(req: &mut Request) -> IronResult<Response> {
         .read_to_string(&mut body)
         .map_err(|e| IronError::new(e, (status::BadRequest, "Error reading request")))?;
     let update: telegram::Update = serde_json::from_str(&body).unwrap();
-    if let Some(msg) = update.message {
+    if let Some(msg) = update.channel_post {
+        if let Some(txt) = msg.text {
+            if txt == format!("/start@{}", read_bot_data(&BotData::Name)) {
+                respond_start(msg.chat.id)?;
+            }
+        }
+    } else if let Some(msg) = update.message {
         if let Some(txt) = msg.text {
             if txt == "/start" {
-                let url = format!("{}{}{}",
-                                  "https://api.telegram.org/bot",
-                                  read_bot_data(BotData::Token),
-                                  "/sendMessage");
-                let params = hashmap![
-                    "chat_id" => format!("{}", msg.chat.id),
-                    "text" => "If the bot was already working, \
-                                you would now have registered yourself. \
-                                Alas, as this is only a placeholder text, \
-                                nothing happened".to_owned(),
-                ];
-                let client = reqwest::Client::new().map_err(|e| {
-                                 IronError::new(e,
-                                                (status::InternalServerError,
-                                                 "Error setting up HTTP client"))
-                             })?;
-                client.post(&url)
-                    .json(&params)
-                    .send()
-                    .unwrap();
+                respond_start(msg.chat.id)?;
+            } else {
+                respond_unknown(msg.chat.id)?;
             }
         }
     }
     Ok(Response::with((status::Ok, "ok")))
+}
+
+fn respond_start(chat_id: i32) -> IronResult<reqwest::Response> {
+    send(chat_id,
+         "If the bot was already working, \
+                    you would now have registered yourself. \
+                    Alas, as this is only a placeholder text, \
+                    nothing happened")
+}
+
+fn respond_unknown(chat_id: i32) -> IronResult<reqwest::Response> {
+    send(chat_id, "Unknown command. Try using /help")
 }
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,9 @@
+extern crate iron;
+extern crate serde_json;
+extern crate reqwest;
+
+use self::iron::prelude::*;
+use self::iron::{status, IronResult};
 use std::env;
 
 pub enum BotData {
@@ -5,19 +11,40 @@ pub enum BotData {
     Name,
 }
 
-pub fn read_bot_data(data: BotData) -> String {
-    let env_var = match data {
+pub fn read_bot_data(data: &BotData) -> String {
+    let env_var = match *data {
         BotData::Token => "TELEGRAM_BOT_TOKEN",
         BotData::Name => "TELEGRAM_BOT_NAME",
     };
-    read_env_var(&env_var)
+    read_env_var(env_var)
 }
 
 
 fn read_env_var(var: &str) -> String {
     env::var_os(var)
         .expect(&format!("{} must be specified. \
-                Did you forget to add it to your .env file?", var))
+                Did you forget to add it to your .env file?",
+                         var))
         .into_string()
         .expect(&format!("{} does not contain a valid UTF8 string", var))
+}
+
+
+pub fn send(chat_id: i32, msg: &str) -> IronResult<reqwest::Response> {
+    let url = format!("{}{}{}",
+                      "https://api.telegram.org/bot",
+                      read_bot_data(&BotData::Token),
+                      "/sendMessage");
+    let params = hashmap![
+                "chat_id" => format!("{}", chat_id),
+                "text" => msg.to_owned(),
+            ];
+    let client = reqwest::Client::new().map_err(|e| {
+                     IronError::new(e,
+                                    (status::InternalServerError, "Error setting up HTTP client"))
+                 })?;
+    client.post(&url)
+        .json(&params)
+        .send()
+        .map_err(|e| IronError::new(e, (status::InternalServerError, "Error sending data")))
 }
